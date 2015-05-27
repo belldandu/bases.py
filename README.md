ad0b9c5f05af81f384bdc45bbb8da29d5dfb72fa
kamijoutouma <michaelfox648@gmail.com>
2015-05-27 00:14:29 -0400

    Add First commit

---
+++ README.md
@@ -0,0 +1,140 @@
+[![Build Status](https://travis-ci.org/kamijoutouma/bases.py.png?branch=master)](https://travis-ci.org/kamijoutouma/bases.py)
+
+# bases.py
+
+A python port for https://github.com/aseemk/bases.js
+
+Utility for converting numbers to/from different bases/alphabets.
+Common bases have convenience aliases (see below), but arbitrary/custom
+alphabets can be used.
+
+## Installation
+
+Python:
+
+```
+For now just download this to the directory you want to use it in. I'll work on a pip for this soon.
+```
+
+## Usage
+
+```py
+from bases import Bases
+bases = Bases()
+
+bases.toBase16(200)                // => 'c8'
+bases.toBase62(99999)              // => 'q0T'
+bases.toAlphabet(300, 'aAbBcC')    // => 'Abba'
+
+bases.fromBase16('c8')               // => 200
+bases.fromBase62('q0T')              // => 99999
+bases.fromAlphabet('Abba', 'aAbBcC') // => 300
+```
+
+## API
+
+Going from numbers to strings:
+
+- `toAlphabet(num, alphabet)`: returns a string representation of the given
+  number for the given alphabet, where the alphabet is an arbitrary string of
+  characters. (See known alphabets below for examples.)
+
+- `toBase(num, base)`: convenience helper for known bases (see below).
+
+- `toBaseX(num)`: convenience helpers for known bases (see below), e.g.
+  `toBase62(num)`.
+
+Going from strings to numbers:
+
+- `fromAlphabet(str, alphabet)`: returns an integer representation of the given
+  string for the given alphabet.
+
+- `fromBase(num, base)`: convenience helper for known bases.
+
+- `fromBaseX(str)`: convenience helpers for known bases.
+
+## Known Bases/Alphabets
+
+Numbers only:
+
+<table>
+<tr>
+<td><strong>Base-2</strong></td>
+<td><code>01</code></td>
+</tr>
+<tr>
+<td><strong>...</strong></td>
+<td><code>012...</code></td>
+</tr>
+<tr>
+<td><strong>Base-10</strong></td>
+<td><code>0123456789</code></td>
+</tr>
+</table>
+
+Letters only:
+
+<table>
+<tr>
+<td><strong>Base-26</strong></td>
+<td><code>abcdefghijklmnopqrstuvwxyz</code></td>
+</tr>
+<tr>
+<td><strong>Base-52</strong></td>
+<td><code>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ</code></td>
+</tr>
+</table>
+
+Alphanumeric:
+
+<table>
+<tr>
+<td><strong>Base-11</strong></td>
+<td><code>0123456789a</code></td>
+</tr>
+<tr>
+<td><strong>...</strong></td>
+<td><code>0123456789ab...</code></td>
+</tr>
+<tr>
+<td><strong>Base-16</strong></td>
+<td><code>0123456789abcdef</code></td>
+</tr>
+<tr>
+<td><strong>Base-36</strong></td>
+<td><code>0123456789abcdefghijklmnopqrstuvwxyz</code></td>
+</tr>
+<tr>
+<td><strong>Base-62</strong></td>
+<td><code>0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ</code></td>
+</tr>
+</table>
+
+Human-friendly:
+
+<table>
+<tr>
+<td><strong>Base-32</strong> (<a href="http://www.crockford.com/wrmg/base32.html" target="_blank">from Douglas Crockford</a>)</td>
+<td><code>0123456789ABCDEFGHJKMNPQRSTVWXYZ</code> (no <code>IOLU</code>)</td>
+</tr>
+<tr>
+<td><strong>Base-58</strong> (<a href="http://www.flickr.com/groups/api/discuss/72157616713786392/" target="_blank">from Flickr</a>)</td>
+<td><code>123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ</code> (no <code>0lIO</code>)</td>
+</tr>
+</table>
+
+Other:
+
+<table>
+<tr>
+<td><strong>Base-64</strong> (<a href="http://en.wikipedia.org/wiki/Base64" target="_blank">as standardized</a>)</td>
+<td><code>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/</code></td>
+</tr>
+</table>
+
+Base-64 warning: besides there being several different standards, padding isn't currently added and line lengths aren't tracked. Not recommended for use with APIs that expect formal base-64 strings!
+
+## License
+
+[MIT license](http://touma.mit-license.org/). (c) 2015 Kamijou Touma
+and [contributors](https://github.com/kamijoutouma/bases.py/graphs/contributors).
---
+++ bases.py
@@ -0,0 +1,106 @@
+# bases.py
+# python port for https://github.com/aseemk/bases.js
+# Utility for converting numbers to/from different bases/alphabets.
+# See README.md for details.
+
+import math
+import re
+
+class Bases(object):
+
+	# init
+	def __init__(self):
+
+		# Known alphabets:
+		self.NUMERALS = '0123456789'
+		self.LETTERS_LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'
+		self.LETTERS_UPPERCASE = self.LETTERS_LOWERCASE.upper()
+		self.KNOWN_ALPHABETS = {}
+
+		# Each of the number ones, starting from base-2 (base-1 doesn't make sense?):
+		i = 2
+		while i <= 10:
+			self.KNOWN_ALPHABETS[i] = self.NUMERALS[:i]
+			i += 1
+
+		# python's native hex is 0-9 followed by *lowercase* a-f, so we'll take that
+		# approach for everything from base-11 to base-16:
+		i = 11
+		while i <= 16:
+			self.KNOWN_ALPHABETS[i] = self.NUMERALS + self.LETTERS_LOWERCASE[:i - 10]
+			i += 1
+
+		# We also model base-36 off of that, just using the full letter alphabet:
+		self.KNOWN_ALPHABETS[36] = self.NUMERALS + self.LETTERS_LOWERCASE
+
+		# And base-62 will be the uppercase letters added:
+		self.KNOWN_ALPHABETS[62] = self.NUMERALS + self.LETTERS_LOWERCASE + self.LETTERS_UPPERCASE
+
+		# For base-26, we'll assume the user wants just the letter alphabet:
+		self.KNOWN_ALPHABETS[26] = self.LETTERS_LOWERCASE
+
+		# We'll also add a similar base-52, just letters, lowercase then uppercase:
+		self.KNOWN_ALPHABETS[52] = self.LETTERS_LOWERCASE + self.LETTERS_UPPERCASE
+
+		# Base-64 is a formally-specified alphabet that has a particular order:
+		# http://en.wikipedia.org/wiki/Base64 (and Python follows this too)
+		# TODO FIXME But our code above doesn't add padding! Don't use this yet...
+		self.KNOWN_ALPHABETS[64] = self.LETTERS_UPPERCASE + self.LETTERS_LOWERCASE + self.NUMERALS + '+/';
+
+		# Flickr and others also have a base-58 that removes confusing characters, but
+		# there isn't consensus on the order of lowercase vs. uppercase... =/
+		# http://www.flickr.com/groups/api/discuss/72157616713786392/
+		# https://en.bitcoin.it/wiki/Base58Check_encoding#Base58_symbol_chart
+		# https://github.com/dougal/base58/blob/master/lib/base58.rb
+		# http://icoloma.blogspot.com/2010/03/create-your-own-bitly-using-base58.html
+		# We'll arbitrarily stay consistent with the above and using lowercase first:
+		self.KNOWN_ALPHABETS[58] = re.sub(r'[0OlI]', '', self.KNOWN_ALPHABETS[62])
+
+		# And Douglas Crockford shared a similar base-32 from base-36:
+		# http://www.crockford.com/wrmg/base32.html
+		# Unlike our base-36, he explicitly specifies uppercase letters
+		self.KNOWN_ALPHABETS[32] = self.NUMERALS + re.sub(r'[ILOU]', '', self.LETTERS_UPPERCASE)
+
+		# Do this for all known alphabets:
+		for base in self.KNOWN_ALPHABETS:
+			if base in self.KNOWN_ALPHABETS:
+				self.makeAlias(base, self.KNOWN_ALPHABETS[base])
+
+	# Returns a stringing representation of the given number for the given alphabet:
+	def toAlphabet(self, num, alphabet):
+		base = len(alphabet)
+	  
+		digits = []# these will be in reverse order since arrays are stacks
+		# execute at least once, even if num is 0, since we should return the '0':
+		while num >= 0:
+			digits.append(num % base)# TODO handle negatives properly?
+			num = math.floor(num / base)
+		chars = [];
+		while len(digits):
+			chars.append(alphabet[int(digits.pop())])
+		return ''.join(chars)
+
+	#Returns an integer representation of the given stringing for the given alphabet:
+	def fromAlphabet(self, string, alphabet):
+		base = len(alphabet)
+		pos = 0
+		num = 0
+		c = False
+		while len(string):
+			c = string[len(string) - 1]
+			string = string[:len(string) - 1]
+			num += math.pow(base, pos) * alphabet.index(c)
+			pos += 1
+		return int(num)
+
+	# And a generic alias too:
+	def toBase(self, num, base):
+		return self.toAlphabet(num, self.KNOWN_ALPHABETS[base])
+
+	def fromBase(self, string, base):
+		return self.fromAlphabet(string, self.KNOWN_ALPHABETS[base])
+
+	# Closure helper for convenience aliases like bases.toBase36():
+	def makeAlias(self, base, alphabet):
+		setattr(self, 'toBase' + str(base), lambda num: self.toAlphabet(num, alphabet))
+		setattr(self, 'fromBase' + str(base), lambda string: self.fromAlphabet(string, alphabet))
