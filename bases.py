1b7445e9d20b4c353daf42b2bce7c8aa717f64b8
kamijoutouma <michaelfox648@gmail.com>
2015-12-08 08:41:27 -0500

    Use one naming scheme and fix little pet peeves
    Change the naming scheme so everything uses one naming scheme and fix little things that were left in by mistake.

--- bases.py
+++ bases.py
@@ -13,40 +13,40 @@ class Bases(object):
 	def __init__(self):
 
 		# Known alphabets:
-		self.NUMERALS = string.digits
-		self.LETTERS_LOWERCASE = string.ascii_lowercase
-		self.LETTERS_UPPERCASE = self.LETTERS_LOWERCASE.upper()
-		self.KNOWN_ALPHABETS = {}
+		self.numerals = string.digits
+		self.lettersLowercase = string.ascii_lowercase
+		self.lettersUppercase = string.ascii_uppercase
+		self.knownAlphabets = {}
 
 		# Each of the number ones, starting from base-2 (base-1 doesn't make sense?):
 		i = 2
 		while i <= 10:
-			self.KNOWN_ALPHABETS[i] = self.NUMERALS[:i]
+			self.knownAlphabets[i] = self.numerals[:i]
 			i += 1
 
 		# python's native hex is 0-9 followed by *lowercase* a-f, so we'll take that
 		# approach for everything from base-11 to base-16:
 		i = 11
 		while i <= 16:
-			self.KNOWN_ALPHABETS[i] = self.NUMERALS + self.LETTERS_LOWERCASE[:i - 10]
+			self.knownAlphabets[i] = self.numerals + self.lettersLowercase[:i - 10]
 			i += 1
 
 		# We also model base-36 off of that, just using the full letter alphabet:
-		self.KNOWN_ALPHABETS[36] = self.NUMERALS + self.LETTERS_LOWERCASE
+		self.knownAlphabets[36] = self.numerals + self.lettersLowercase
 
 		# And base-62 will be the uppercase letters added:
-		self.KNOWN_ALPHABETS[62] = self.NUMERALS + self.LETTERS_LOWERCASE + self.LETTERS_UPPERCASE
+		self.knownAlphabets[62] = self.numerals + self.lettersLowercase + self.lettersUppercase
 
 		# For base-26, we'll assume the user wants just the letter alphabet:
-		self.KNOWN_ALPHABETS[26] = self.LETTERS_LOWERCASE
+		self.knownAlphabets[26] = self.lettersLowercase
 
 		# We'll also add a similar base-52, just letters, lowercase then uppercase:
-		self.KNOWN_ALPHABETS[52] = self.LETTERS_LOWERCASE + self.LETTERS_UPPERCASE
+		self.knownAlphabets[52] = self.lettersLowercase + self.lettersUppercase
 
 		# Base-64 is a formally-specified alphabet that has a particular order:
 		# http://en.wikipedia.org/wiki/Base64 (and Python follows this too)
 		# TODO FIXME But our code above doesn't add padding! Don't use this yet...
-		self.KNOWN_ALPHABETS[64] = self.LETTERS_UPPERCASE + self.LETTERS_LOWERCASE + self.NUMERALS + '+/';
+		self.knownAlphabets[64] = self.lettersUppercase + self.lettersLowercase + self.numerals + '+/'
 
 		# Flickr and others also have a base-58 that removes confusing characters, but
 		# there isn't consensus on the order of lowercase vs. uppercase... =/
@@ -55,17 +55,17 @@ class Bases(object):
 		# https://github.com/dougal/base58/blob/master/lib/base58.rb
 		# http://icoloma.blogspot.com/2010/03/create-your-own-bitly-using-base58.html
 		# We'll arbitrarily stay consistent with the above and using lowercase first:
-		self.KNOWN_ALPHABETS[58] = re.sub(r'[0OlI]', '', self.KNOWN_ALPHABETS[62])
+		self.knownAlphabets[58] = re.sub(r'[0OlI]', '', self.knownAlphabets[62])
 
 		# And Douglas Crockford shared a similar base-32 from base-36:
 		# http://www.crockford.com/wrmg/base32.html
 		# Unlike our base-36, he explicitly specifies uppercase letters
-		self.KNOWN_ALPHABETS[32] = self.NUMERALS + re.sub(r'[ILOU]', '', self.LETTERS_UPPERCASE)
+		self.knownAlphabets[32] = self.numerals + re.sub(r'[ILOU]', '', self.lettersUppercase)
 
 		# Do this for all known alphabets:
-		for base in self.KNOWN_ALPHABETS:
-			if base in self.KNOWN_ALPHABETS:
-				self.makeAlias(base, self.KNOWN_ALPHABETS[base])
+		for base in self.knownAlphabets:
+			if base in self.knownAlphabets:
+				self.makeAlias(base, self.knownAlphabets[base])
 
 	# Returns a stringing representation of the given number for the given alphabet:
 	def toAlphabet(self, num, alphabet):
@@ -83,27 +83,27 @@ class Bases(object):
 			chars.append(alphabet[int(digits.pop())])
 		return ''.join(chars)
 
-	#Returns an integer representation of the given stringing for the given alphabet:
-	def fromAlphabet(self, string, alphabet):
+	# Returns an integer representation of the given stringing for the given alphabet:
+	def fromAlphabet(self, strRep, alphabet):
 		base = len(alphabet)
 		pos = 0
 		num = 0
 		c = False
-		while len(string):
-			c = string[len(string) - 1]
-			string = string[:len(string) - 1]
+		while len(strRep):
+			c = strRep[len(strRep) - 1]
+			strRep = strRep[:len(strRep) - 1]
 			num += math.pow(base, pos) * alphabet.index(c)
 			pos += 1
 		return int(num)
 
 	# And a generic alias too:
 	def toBase(self, num, base):
-		return self.toAlphabet(num, self.KNOWN_ALPHABETS[base])
+		return self.toAlphabet(num, self.knownAlphabets[base])
 
-	def fromBase(self, string, base):
-		return self.fromAlphabet(string, self.KNOWN_ALPHABETS[base])
+	def fromBase(self, strRep, base):
+		return self.fromAlphabet(strRep, self.knownAlphabets[base])
 
 	# Closure helper for convenience aliases like bases.toBase36():
 	def makeAlias(self, base, alphabet):
 		setattr(self, 'toBase' + str(base), lambda num: self.toAlphabet(num, alphabet))
-		setattr(self, 'fromBase' + str(base), lambda string: self.fromAlphabet(string, alphabet))
+		setattr(self, 'fromBase' + str(base), lambda strRep: self.fromAlphabet(strRep, alphabet))
