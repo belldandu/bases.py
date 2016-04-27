d15378e92871e17dfe6de64f765d2a089866e388
Sorch <support@sorch.info>
2016-04-27 11:41:45 +0100

    Added setup.py for the ability to install on other computers

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='bases.py',
-      version='0.1.6',
+      version='0.2.2',
       description='A python port for https://github.com/aseemk/bases.js',
       author='KamijouTouma',
-      author_email='KamijouTouma@ilp.moe',
+      author_email='Kami@ilp.moe',
       url='https://github.com/kamijoutouma/bases.py/',
       packages=['bases'],
       package_dir={'bases': 'src'}
